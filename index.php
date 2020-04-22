2f926f318dac9e11b70190302b45dc1d95a28d03
ratezou <60992623+ratezou@users.noreply.github.com>
2020-04-22 14:54:14 +0900

    a

--- index.php
+++ index.php
@@ -5,19 +5,17 @@
   <link rel="stylesheet" href="style.css">
 <head>
 <body>
-  <div id="container">
-    <div id="header"></div>
-    <div id="login">
-      <form action="" method="post">
-        <h1>ログイン</h1>
-        <h2>社員ID</h2>
-        <input type="text" placeholder="社員ID" required>
-        <h2>パスワード</h2>
-        <input type="password" placeholder="パスワード" required>
-        <button>Login</button>
-      </form>
-    </div>
-    <div id="footer"></div>
+<header>
+  <h1>ログイン</h1>
+</header>
+<div id="container">
+  <div id="login">
+      <input type="text" id="id" placeholder="社員ID" required>
+      <input type="password" id="pass" placeholder="パスワード" required>
+      <button>Login</button>
+    </form>
   </div>
+<footer>footer</footer>
+</div>
 </body>
 </html>
