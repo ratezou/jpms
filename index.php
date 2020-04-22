06c5d803050e9a5c9ff7c5791f159780f1dca188
ratezou <60992623+ratezou@users.noreply.github.com>
2020-04-22 15:10:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,23 @@
   <link rel="stylesheet" href="style.css">
 <head>
 <body>
+
 <header>
   <h1>ログイン</h1>
 </header>
-<div id="container">
-  <div id="login">
-      <input type="text" id="id" placeholder="社員ID" required>
-      <input type="password" id="pass" placeholder="パスワード" required>
-      <button>Login</button>
-    </form>
-  </div>
-<footer>footer</footer>
+
+  <!--社員ID-->
+  <label for="id">社員ID：</label>
+  <input type="text" id="id" placeholder="社員ID" required>
+  <!--パスワード-->
+  <input type="password" id="pass" placeholder="パスワード" required>
+  <!--ボタン-->
+  <button>Login</button>
+
+<footer>
+  footer
+</footer>
+
 </div>
 </body>
 </html>
