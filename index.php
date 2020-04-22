04054eb96b848a3fa42b61736b8401692f441a2c
ratezou <60992623+ratezou@users.noreply.github.com>
2020-04-22 13:35:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-
-echo "hello heroku!1";
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>Login</title>
+<head>
+<body>
+<div class="content">
+  <div class="login">
+    <input type="text" name="id" id="id" value="" placeholder="社員ID" required>
+    <input type="password" name="pass" id="pass" placeholder="パスワード" required>
+    <button>Login</button>
+  </div>
+</div>
+</body>
+</html>
