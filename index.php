023961d9ae7c338e0538d2e50013d80f7a6dc7b8
ratezou <60992623+ratezou@users.noreply.github.com>
2020-04-22 13:52:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,22 @@
 <html lang="ja">
 <head>
   <title>Login</title>
+  <style>
+    input {
+      font-size: 3em;
+    }
+  </style>
 <head>
 <body>
 <div class="content">
-  <div class="login">
-    <div clase="id">
-      <input type="text" name="id" id="id" value="" placeholder="社員ID" required>
-    </div>
-    <div class="pass">
-      <input type="password" name="pass" id="pass" placeholder="パスワード" required>
-    </div>
-    <div class="btn">
-      <button>Login</button>
-    </div>
+  <div clase="id">
+    <input type="text" placeholder="社員ID" required>
+  </div>
+  <div class="pass">
+    <input type="password" placeholder="パスワード" required>
+  </div>
+  <div class="btn">
+    <button>Login</button>
   </div>
 </div>
 </body>
