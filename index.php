29f1eaebd95bd4b199a7f1ada58e325bda3d0fff
ratezou <60992623+ratezou@users.noreply.github.com>
2020-04-22 14:45:16 +0900

    a

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
 <html lang="ja">
 <head>
   <title>Login</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="style.css">
 <head>
 <body>
-  <form action="" method="post">
-    <h1>ログイン</h1>
-    <h2>社員ID</h2>
-    <input type="text" placeholder="社員ID" required>
-    <h2>パスワード</h2>
-    <input type="password" placeholder="パスワード" required>
-    <h2> ---</h2>
-    <button>Login</button>
-</form>
+  <div id="container">
+    <div id="header"></div>
+    <div id="login">
+      <form action="" method="post">
+        <h1>ログイン</h1>
+        <h2>社員ID</h2>
+        <input type="text" placeholder="社員ID" required>
+        <h2>パスワード</h2>
+        <input type="password" placeholder="パスワード" required>
+        <button>Login</button>
+      </form>
+    </div>
+    <div id="footer"></div>
+  </div>
 </body>
 </html>
